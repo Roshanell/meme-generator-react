d4c6f4ae86760ce3ee72644fca824b203db2b131
Roshanell Francisco <96213223+Roshanell@users.noreply.github.com>
2023-03-15 14:59:21 -0500

    rendering random images

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,23 @@
 import React from "react";
 import memesData from "../memesData.js";
+import { useState } from "react";
 
 function Form() {
 	//const randomImage = {Math.floor(Math.random() * memesData.data.memes.length)}
+	const [meme, setMeme] = React.useState({
+		topText: "",
+		bottomText: "",
+		randomImage: "http://i.imgflip.com/1bij.jpg",
+	});
+	const [allMemeImages, setAllMemeImages] = useState(memesData);
 	function getMemeImage() {
 		const memesArr = memesData.data.memes;
 		const randomImageIndex = Math.floor(Math.random() * memesArr.length);
 		const url = memesArr[randomImageIndex].url;
+		setMeme((prevMeme) => ({
+			...prevMeme,
+			randomImage: url,
+		}));
 		console.log(url);
 	}
 	return (
@@ -18,6 +29,7 @@ function Form() {
 					Get a new meme image 🖼
 				</button>
 			</div>
+			<img src={meme.randomImage} className="meme-image" />
 		</main>
 	);
 }
